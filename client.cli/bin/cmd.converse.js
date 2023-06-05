ad847aea77c016a75eedd55254eb059074525eb8
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2023-06-05 19:47:14 +0200

    feat: update packages

--- client.cli/bin/cmd.converse.js
+++ client.cli/bin/cmd.converse.js
@@ -1,63 +1,136 @@
 /** @typedef {import("openai").ChatCompletionRequestMessage} ChatCompletionRequestMessage */
+/** @typedef {import("@z3r0boot/core/models").Message} Message */
+/** @typedef {import("@z3r0boot/core/models").Conversation} Conversation */
 
 import {
   createConversation,
   findAllConversations,
   getOneConversation,
   findAllMessages,
+  createMessage,
+  createTextBlock,
 } from "@z3r0boot/core/models"
 import { getOpenAIClient, getOpenAIModels } from "@z3r0boot/pure-utils"
+import chalk from "chalk"
 import inquirer from "inquirer"
 
-/** @type {ChatCompletionRequestMessage[]} */
-const MESSAGES = []
-
+const MESSAGES = /** @type {Message[]} */ ([])
 const OPENAI_CLIENT = getOpenAIClient("converse-client")
 
 /**
- * @param {object} options
- * @param {string} options.model
- * @returns {Promise<void>}
+ * @param {Message} input
  */
-const askUser = async ({ model }) => {
-  const { message } = await inquirer.prompt({
+const echoMessage = async input => {
+  const actor = "And"
+
+  console.log(`${chalk.bold(actor)}: ${input.content}`)
+  console.log()
+}
+
+/**
+ * @param {object} options
+ * @param {Conversation} options.conversation
+ * @returns {Promise<Message>}
+ */
+const askUser = async ({ conversation }) => {
+  const { question } = await inquirer.prompt({
     type: "input",
-    name: "message",
+    name: "question",
     message: "You: ",
   })
 
-  MESSAGES.push({
+  const message = await createMessage({
+    conversationId: conversation.id,
+    content: question,
     role: "user",
-    content: message,
   })
 
-  const response = await OPENAI_CLIENT.createChatCompletion({
-    model,
-    messages: MESSAGES,
+  await createTextBlock({
+    content: message.content,
+    type: "conversation-excerpt",
+    description: "User prompt",
+    meta: {
+      conversationId: conversation.id,
+    },
   })
 
-  MESSAGES.push({
-    role: "assistant",
-    content: response.data.choices[0].message.content,
-  })
+  MESSAGES.push(message)
 
-  console.log(`AI: ${response.data.choices[0].message.content}`)
-
-  askUser({ model })
+  return message
 }
 
+/**
+ * @param {object} options
+ * @param {Conversation} options.conversation
+ * @param {Message[]} options.messages
+ * @returns {Promise<Message>}
+ */
+const askAssistent = async ({ conversation, messages }) => {
+  const aiResponse = await OPENAI_CLIENT.createChatCompletion({
+    model: conversation.model,
+    messages,
+  })
+
+  if (!aiResponse.data.choices[0]?.message) {
+    throw new Error("No response from AI assistent")
+  }
+
+  const userMessage = messages.findLast(item => item.role === "user")
+
+  if (!userMessage) {
+    throw new Error("No user message found")
+  }
+
+  const aiMessage = await createMessage({
+    conversationId: conversation.id,
+    content: aiResponse.data.choices[0].message.content,
+    role: "assistant",
+  })
+
+  await createTextBlock({
+    content: aiMessage.content,
+    type: "conversation-excerpt",
+    description: "AI's response",
+    meta: {
+      conversationId: conversation.id,
+    },
+  })
+
+  MESSAGES.push(aiMessage)
+
+  return aiMessage
+}
+
+/**
+ * @param {Conversation} conversation
+ * @returns {Promise<void>}
+ */
+const restartConversation = async conversation => {
+  const messages = await findAllMessages({
+    where: {
+      conversationId: conversation.id,
+    },
+  })
+
+  messages.forEach(message => echoMessage(message))
+}
+
+/**
+ * @returns {Promise<void>}
+ */
 export const converse = async () => {
+  const conversations = await findAllConversations()
   const conversationIds = [
-    "Start a new conversation",
-    ...(await findAllConversations()).map(item => item.id),
+    "New conversation",
+    ...conversations.map(item => item.id),
   ]
 
   const { conversationId } = await inquirer.prompt({
     type: "list",
     name: "conversationId",
-    message: "Continue a previous conversation:",
+    message: "Continue conversation:",
     choices: conversationIds,
-    default: "Start a new conversation",
+    default: "New conversation",
   })
 
   const { model } = await inquirer.prompt({
@@ -73,14 +146,5 @@ export const converse = async () => {
       ? await createConversation({ model })
       : await getOneConversation(conversationId)
 
-  const messages = findAllMessages({
-    where: { conversationId: conversation.id },
-  })
-
-  MESSAGES.push({
-    role: "system",
-    content: `You are talking to OpenAI's ${model}.`,
-  })
-
-  askUser({ model })
+  restartConversation(conversation)
 }
