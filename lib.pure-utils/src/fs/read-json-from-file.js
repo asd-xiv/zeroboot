02fb88c4e62aaefd7deab8341e7be96c55eb59ee
Andrei Dumitrescu <5057797+andreidmt@users.noreply.github.com>
2023-07-15 08:21:55 +0200

    feat: add sync version of readJSONFromFile

--- lib.pure-utils/src/fs/read-json-from-file.js
+++ lib.pure-utils/src/fs/read-json-from-file.js
@@ -1,10 +1,20 @@
+import { readFileSync } from "node:fs"
 import { readFile } from "node:fs/promises"
 
 /**
  * Read a file and parse it as JSON.
- * @template {Record<string, unknown>} T
+ * @template {Record<string, unknown> | undefined} T
  * @param {string} input
  * @returns {Promise<T>}
  */
 export const readJSONFromFile = input =>
   readFile(input, "utf8").then(buffer => JSON.parse(buffer.toString()))
+
+/**
+ * Read a file and parse it as JSON.
+ * @template {Record<string, unknown> | undefined} T
+ * @param {string} input
+ * @returns {T}
+ */
+export const readJSONFromFileSync = input =>
+  JSON.parse(readFileSync(input, "utf8"))
